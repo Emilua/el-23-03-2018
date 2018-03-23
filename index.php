23f9b53c5ce26d45cb74cee83bea906bad054955
somebody <somebody@smd.com>
2018-03-23 20:05:13 +0200

    index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Первый проект</title>
+    </head>
+    <body>
+        <p>Проектов много, но такой проект один - он мой!</p>
+    </body>
+</html>
